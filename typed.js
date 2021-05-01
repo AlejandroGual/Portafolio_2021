6911bb0539c53e75f497ef77a69331ed817ba2ff
Geovanny Gualotuña <geova12.gual@gmail.com>
2021-04-30 22:28:47 -0500

    nuevo diseño de pagina

---
+++ typed.js
@@ -0,0 +1,19 @@
+var typed = new Typed('.typed', {
+	strings: [
+		'<i class="nombre">Soy Geovanny </i>',
+        
+		
+	],
+	stringsElement: '#cadenas-texto', 
+	typeSpeed: 75,
+	startDelay: 300, 
+	backSpeed: 75, 
+	smartBackspace: true, 
+	shuffle: false, 
+	backDelay: 1500, 
+	loop: true, 
+	loopCount: false,
+	showCursor: true, 
+	cursorChar: '|', 
+	contentType: 'html', 
+});
